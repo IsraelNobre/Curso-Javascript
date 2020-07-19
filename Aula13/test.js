215d107b0011c4df3d729d5211f09035b09f251a
Israel Nobre Oliveira da Silva <nobreisrael@gmail.com>
2020-07-19 12:52:11 -0300

    att

---
+++ Aula13/test.js
@@ -0,0 +1,21 @@
+var c = 1
+while (c <= 500) {
+   console.log('Tudo Bem')
+   c++
+}
+
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')*/
+
+
+
