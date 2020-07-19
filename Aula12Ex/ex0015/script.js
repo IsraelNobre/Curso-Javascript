73ca91b6f541b11188c50c6f3d1d8376038bbcb3
Israel Nobre Oliveira da Silva <nobreisrael@gmail.com>
2020-07-19 09:17:45 -0300

    att

--- Aula12Ex/ex0015/script.js
+++ Aula12Ex/ex0015/script.js
@@ -8,7 +8,43 @@ function  verificar() {
     } else {
       var fsex = document.getElementsByName ('radsex')
       var idade = ano - Number(fano.value)
-      res.innerHTML =   `Idade calculada: ${idade}`
+      var gênero = ''
+      var img = document.createElement('img')
+      img.setAttribute('id', 'foto')
+      if (fsex[0].checked) {
+        gênero = 'Homem'
+        if (idade >=0 && idade <10){
+        //Criança
+        img.setAttribute ('src','bebem-1.png')
+      } else if (idade <21){
+        // jovem
+        img.setAttribute ('src','menino.png')
+      } else if (idade < 50){
+        // adulto
+        img.setAttribute ('src','jovemm.png')
+      } else {
+        //idoso
+        img.setAttribute ('src','idoso-1.png')
+      }
+      } else if (fsex[1].checked){
+        gênero = 'Mulher'
+        if (idade >=0 && idade <10){
+          // Criança
+          img.setAttribute ('src','bebef-1.png')
+        } else if (idade <21){
+          // jovem
+          img.setAttribute ('src','menina.png')
+        } else if (idade <50){
+          //adulto
+          img.setAttribute ('src','jovemf-1.png')
+        } else{
+          //idoso
+          img.setAttribute ('src','idosa-1.png')
+        }
+      }
+      res.style.textAlign = 'center'
+      res.innerHTML = `Dectetamos ${gênero} com ${idade} anos.`
+      res.appendChild(img)
      
     }
 
